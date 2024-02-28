881bf976ea3ef92830310aa17c1c6d941d9f28c9
Lagzen Thakuri <thakurizen2@gmail.com>
2024-02-28 10:33:36 +0545

    basic c expalined

--- basic-c/index.c
+++ basic-c/index.c
@@ -1,6 +1,7 @@
 
-#include<stdio.h>
+#include<stdio.h> //  standard input/output library  to provide input and output operations.
  
- int main(){
-    printf("hello world");
- }
+ int main()      //The int before main indicates that the function returns an integer value.
+ {        // starting main function.
+    printf("hello world");  //to print formatted output to the console.
+ }          // closing main function.
